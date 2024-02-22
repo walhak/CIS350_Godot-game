b69375b07195a6d5cd3466f86a859b226d469224
Nazim Jafarov <djafarovnazim@gmail.com>
2024-02-22 01:07:46 -0500

    Options menu modified and new features added

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -3,5 +3,8 @@ extends Control
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Levels/Level_1/Map1.tscn")
 
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://Menu/settings.tscn")
+
 func _on_quit_pressed():
 	get_tree().quit()
