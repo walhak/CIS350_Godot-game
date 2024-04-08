1032d5ace02472be2ff46f620a16b714d697f336
Malachi Hanson <hansomal@mail.gvsu.edu>
2024-04-08 10:25:09 -0400

    added commits and reset button works

--- GlobalCharacterMovement.gd
+++ GlobalCharacterMovement.gd
@@ -21,33 +21,42 @@ extends CharacterBody2D
 #currently how many jumps the character did
 var current_jumps = 0
 
+#name of each level
 var rep_cur = ["M1", "M2", "menu"]
+#path to each level
 var current_level = ["res://Levels/Level_1/Map1.tscn", "res://Levels/Level_2/Map2.tscn", "res://Menu/menu.tscn"]
 
+#check if character is on the odor
 var check = false
 
 #main update to allow movement
 func _physics_process(delta):
 	var input_dir: Vector2 = input()
 	
+	#how the character resets on each level and progresses to next level
 	if get_tree().get_current_scene() != null:
+		#tracks what level character on
 		var current_scene_name = get_tree().get_current_scene().get_name()
 		var index = rep_cur.find(current_scene_name)
 		
+		#moves character to next level pressing (s)
 		if Input.is_action_just_pressed("move_down") and check == true:
 			current_scene_name = get_tree().get_current_scene().get_name()
 			index = rep_cur.find(current_scene_name)
 			var next_scene_path = current_level[index+1]
 			get_tree().change_scene_to_file(next_scene_path)
 			
+		#allows character to reset pressing (r)
 		if Input.is_action_just_pressed("resetButton"):
 			current_scene_name = get_tree().get_current_scene().get_name()
 			index = rep_cur.find(current_scene_name)
 			get_tree().change_scene_to_file(current_level[index])
 	
+	#allows character to quit game pressing (esc)
 	if Input.is_action_just_pressed("exitGame"):
 		get_tree().quit()
 		
+	#updates character movement
 	if input_dir != Vector2.ZERO:
 		acclerate(input_dir)
 	else:
@@ -91,11 +100,11 @@ func jump():
 	if is_on_floor():
 		current_jumps = 0
 		
-#how character moves to next level
+#how character moves to next level and check if they are in the doorway
 func _on_area_2d_body_entered(body):
 	if body == self:
 		check = true
-
-
+		
+#checks if character left the doorway to disable the movement to next level
 func _on_area_2d_body_exited(body):
 	check = false
--- Menu/menu.gd
+++ Menu/menu.gd
@@ -1,10 +1,13 @@
 extends Control
 
+#(play) button moves player to first level
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Levels/Level_1/Map1.tscn")
 
+#(option) button moves player to settings screen
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://Menu/settings.tscn")
 
+#(quit) button allows player to leave game
 func _on_quit_pressed():
 	get_tree().quit()
